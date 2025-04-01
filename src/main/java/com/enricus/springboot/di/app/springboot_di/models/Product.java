adcac414d9c63e8772d4908d93547af09271f7b6
kikux-ubuntu <i.am.kike@gmail.com>
2025-03-31 23:45:19 -0600

    first commit

---
+++ src/main/java/com/enricus/springboot/di/app/springboot_di/models/Product.java
@@ -0,0 +1,42 @@
+package com.enricus.springboot.di.app.springboot_di.models;
+
+public class Product {
+
+    private Long id;
+    private Long price;
+    private String name;
+    
+    public Product() {
+    }
+    
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Long getPrice() {
+        return price;
+    }
+    
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/enricus/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,27 @@
+ package com.enricus.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.enricus.springboot.di.app.springboot_di.models.Product;
+import com.enricus.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+    //this class acces data via the repository. it may make things to data or use other repos
+
+    private ProductRepository repo = new ProductRepository(); //this attr is shared by all instances or users, because the controller is a singleton
+
+    public List<Product> findAll() {
+        //multiply by a tax
+        return repo.findAll().stream().map(p -> { //map generates new list: inmutability
+            Double priceTaxed = p.getPrice() * 1.25d;
+            p.setPrice(priceTaxed.longValue()); //this is using the same object: mutability
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long Id) {
+        return repo.findById(Id);
+    }
+}
